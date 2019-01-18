7ddbf0e8f21d8c0646e35b06db700f55aa64f7ae
Radek <radoslaw.biesek@gmail.com>
2019-01-18 20:29:32 +0100

    Add template for upload

--- modules/handlers.js
+++ modules/handlers.js
@@ -5,10 +5,11 @@ exports.upload = function(request, response) {
     var form = new formidable.IncomingForm();
     form.parse(request, function(error, fields, files) {
         fs.renameSync(files.upload.path, "test.png");
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("received image:<br/>");
-        response.write("<img src='/show' />");
-        response.end();
+        fs.readFile('./templates/upload.html', function(err, html) {
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(html);
+            response.end();            
+        })
     });
 }
 
